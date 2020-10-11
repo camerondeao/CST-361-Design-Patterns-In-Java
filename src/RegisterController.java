1b55923659034774c3e88005a620d78d29546d20
cdeao <cmdeao16@gmail.com>
2020-10-10 22:45:51 -0500

    Created initial storage and retrieval of users within the database.

--- src/RegisterController.java
+++ src/RegisterController.java
@@ -1,6 +1,9 @@
+import java.sql.DriverManager;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import java.sql.*;
 
 import beans.User;
 
@@ -9,10 +12,38 @@ import beans.User;
 public class RegisterController {
 
 	public String onSubmit(User user) {
+		
+		StoreUser(user);
 		//Forwards to view along with the user bean
 		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
 
 		return "registerResponse.xhtml";
 	}
 	
+	public void StoreUser(User user)
+	{
+		try
+		{
+			
+			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sys", "root", "connection");
+			String sqlStatement = "INSERT INTO users (firstname, lastname, age, state, emailaddress, username, password) VALUES (?,?,?,?,?,?,?)";
+			
+			PreparedStatement prep = myConn.prepareStatement(sqlStatement);
+			prep.setString(1, user.getFirstName());
+			prep.setString(2, user.getLastName());
+			prep.setInt(3, user.getAge());
+			prep.setString(4, user.getState());
+			prep.setString(5, user.getEmail());
+			prep.setString(6, user.getUsername());
+			prep.setString(7, user.getPassword());
+			
+			prep.executeUpdate();
+			
+			myConn.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
--- src/data/UserDataService.java
+++ src/data/UserDataService.java
@@ -1,12 +1,44 @@
 package data;
 
-public class UserDataService {
-	public boolean login(String username, String password) {
-		if (username.compareTo("dev") == 0 && password.compareTo("test") == 0) {
+import java.sql.*;
+
+public class UserDataService 
+{
+	public boolean login(String username, String password) 
+	{
+		
+		System.out.println("Username: " + username + " Password: " + password);
+		if(checkLogin(username, password))
+		{
 			return true;
 		}
-		else {
+		else 
+		{
 			return false;
 		}
+	}
+	
+	public boolean checkLogin(String username, String password)
+	{
+		try
+		{
+			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sys", "root", "connection");	
+			PreparedStatement statement = myConn.prepareStatement("SELECT * from users WHERE username = ?");
+			statement.setString(1, username);
+			ResultSet rs = statement.executeQuery();
+			
+			while(rs.next())
+			{			
+				if(rs.getString("username").equals(username) && rs.getString("password").equals(password))
+				{
+					return true;
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
